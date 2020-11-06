d2d17e665f7ef459f77afb3d06af2ea329197cbd
Eloisa Amorin <eloisa.amorin@keyrus.com.br>
2020-11-06 17:33:06 -0300

    Completed the header

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,5 +1,20 @@
-import '../styles/styles.css'
+import '../styles/styles.css';
+import Person from './modules/Person';
 
 if (module.hot) {
     module.hot.accept();
 }
+
+/* Lesson example code below this line */
+class Adult extends Person {
+    payTaxes() {
+        console.log(this.name + "now owes zero taxes.")
+    }
+}
+
+let john = new Person("John Doe", "blue");
+john.greet();
+
+let jane = new Adult("Jane Smith", "blue");
+jane.greet();
+jane.payTaxes();
